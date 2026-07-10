ef70e50811769d5d01ef27104ef5e14958c8708e
kilyong jeong <uzoolove@gmail.com>
2026-07-10 14:52:02 +0900

    스프링 부트 initializr, intellij 프로젝트 생성

--- SpringCore/src/main/java/net/likelion/bebc25/spring/lifecycle/TempFileSupport2.java
+++ SpringCore/src/main/java/net/likelion/bebc25/spring/lifecycle/TempFileSupport2.java
@@ -1,24 +1,25 @@
 package net.likelion.bebc25.spring.lifecycle;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 // 임시로 파일을 만들어서 파일에 로그를 출력
-//@Component
-public class TempFileSupport {
+@Component
+public class TempFileSupport2 implements InitializingBean, DisposableBean {
     @Value("resources/temp.log")
     private String filePath; // 파일 경로
 
-    public TempFileSupport(){
+    public TempFileSupport2(){
         System.out.println("생성자 호출됨. " + filePath);
     }
 
     // 네트워크, 파일시스템 연결 같은 무거운 작업을 수행할 초기화 메서드 정의
     // 비즈니스 메서드 호출 전에 실행해야 할 작업
-    @PostConstruct // 초기화 콜백
-    public void init(){
+    // 초기화 콜백
+    @Override
+    public void afterPropertiesSet() throws Exception {
         System.out.println(filePath + " 경로의 FileOutputStream 생성.");
     }
 
@@ -26,8 +27,9 @@ public class TempFileSupport {
         System.out.println(filePath + "에 로그 저장: " + msg);
     }
 
-    @PreDestroy // 소멸 콜백
-    public void close(){
+    // 소멸 콜백
+    @Override
+    public void destroy() throws Exception {
         System.out.println(filePath + " 경로의 FileOutputStream 닫기.");
     }
 }

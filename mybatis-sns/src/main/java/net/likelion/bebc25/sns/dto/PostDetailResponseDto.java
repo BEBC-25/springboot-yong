fe36bca370d031047672d2c2b3d1de98198d76fd
kilyong jeong <uzoolove@gmail.com>
2026-09-02 16:32:29 +0900

    mybatis 완료

--- mybatis-sns/src/main/java/net/likelion/bebc25/sns/dto/PostDetailResponseDto.java
+++ mybatis-sns/src/main/java/net/likelion/bebc25/sns/dto/PostDetailResponseDto.java
@@ -4,17 +4,14 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
 public class PostDetailResponseDto {
     private Long id;
     private String content;
--- mybatis-sns/src/main/java/net/likelion/bebc25/sns/mapper/PostMapper.java
+++ mybatis-sns/src/main/java/net/likelion/bebc25/sns/mapper/PostMapper.java
@@ -1,11 +1,14 @@
 package net.likelion.bebc25.sns.mapper;
 
 import net.likelion.bebc25.sns.dto.PostCreateDto;
+import net.likelion.bebc25.sns.dto.PostDetailResponseDto;
 import net.likelion.bebc25.sns.dto.PostResponseDto;
+import net.likelion.bebc25.sns.dto.PostSearchCondition;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface PostMapper {
@@ -23,4 +26,23 @@ public interface PostMapper {
 
     // 5. 단일 기본형 단건 삭제
     void deleteById(@Param("id") Long id);
+
+    // 6. 복합 ResultMap 조인 상세 조회 (게시글 + 작성자 + 댓글 목록)
+    PostDetailResponseDto findPostDetailById(Long id);
+
+    // 7. 다중 조건 동적 검색 (<where>, <if>)
+    List<PostResponseDto> searchPosts(PostSearchCondition condition);
+
+    // 8. 동적 정렬 분기 조회 (<choose>, <when>, <otherwise>)
+    List<PostResponseDto> findPostsWithSort(PostSearchCondition condition);
+
+    // 9. 동적 부분 수정 (<set>, <if>)
+    void updateSelective(Map<String, Object> params);
+
+    // 10. 다중 ID 일괄 삭제 (<foreach>)
+    void deleteByIds(@Param("idList") List<Long> idList);
+
+    // 11. 공통 SQL 조각 재사용 조회 (<sql>, <include>)
+    PostResponseDto findByIdWithInclude(@Param("id") Long id);
+
 }
